adbf543a8adccd800284cfa631a3cd793ed2988f
HUMANACTION\Krzysztof <jksikora90@gmail.com>
2017-10-25 17:23:40 +0200

    Changed the agenda to reflect the reality and added created solution

---
+++ LunchService/LunchService/LunchDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace LunchService
+{
+    public class LunchDbContext : DbContext
+    {
+        public LunchDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ LunchService/LunchService/Models/Meal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LunchService.Models
+{
+    public class Meal
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
